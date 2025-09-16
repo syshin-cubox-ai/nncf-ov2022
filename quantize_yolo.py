a34a466fcb3e5fa0d0955aae15163f9c6147c8ad
신석용 <syshin@secern.ai>
2025-09-16 10:22:19 +0900

    Update create_conda_env.txt

--- quantize_yolo.py
+++ quantize_yolo.py
@@ -63,9 +63,9 @@ def transform_fn(data_item) -> np.ndarray:
 
 def main():
     # Args
-    pt_path = Path("runs/detect/bezel_detector/weights/last.pt").resolve(strict=True)
+    pt_path = Path("runs/detect/train4/weights/last.pt").resolve(strict=True)
     data_yaml_path = Path(
-        "../../data/home-collected-bezel-background-v5/data.yaml"
+        "../../data/home-collected-bezel-paper-background-v8/data.yaml"
     ).resolve(strict=True)
     subset_size = 300
     fast_bias_correction = False
